794af9dfbfa239033df3d3b19a797411e7936f04
TeslaWorks239 <gcherepennicov@gmail.com>
2020-04-28 00:30:56 +0300

    push1

--- main/views.py
+++ main/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from webpush import send_user_notification
 
 # Create your views here.
-def index(request):
-    payload = {"head": "Welcome!", "body": "Hello World"}
-    send_user_notification(user=user, payload=payload, ttl=1000)
+def send_push(head,body,who):
+    payload = {"head": head, "body": body}
+    send_user_notification(user=who, payload=payload, ttl=1000)
