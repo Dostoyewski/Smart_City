12a85e820acfef61e12f8b0f5bea7ced1dfb7aca
Dostoyewski <dostoyewski@yandex.ru>
2020-04-27 13:19:29 +0300

    Added base template

--- smart_city/urls.py
+++ smart_city/urls.py
@@ -1,4 +1,4 @@
-"""smart_city URL Configuration
+"""Smart_City URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- smart_city/wsgi.py
+++ smart_city/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for smart_city project.
+WSGI config for Smart_City project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_city.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Smart_City.settings')
 
 application = get_wsgi_application()
