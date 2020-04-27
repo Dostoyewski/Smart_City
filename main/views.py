a717a3c4c27cd24173263da57b97594f62758756
TeslaWorks239 <gcherepennicov@gmail.com>
2020-04-28 00:29:43 +0300

    push

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from webpush import send_user_notification
 
 # Create your views here.
+def index(request):
+    payload = {"head": "Welcome!", "body": "Hello World"}
+    send_user_notification(user=user, payload=payload, ttl=1000)
--- smart_city/urls.py
+++ smart_city/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url, re_path
 from allauth.account import views as auth_views
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -43,4 +44,6 @@ urlpatterns = [
     path('accounts/password/reset/key/done/',
          auth_views.PasswordResetFromKeyDoneView.as_view(template_name='account/password_reset_from_key_done.html'),
          name="account_reset_password_from_key_done"),
+     url(r'^webpush/', include('webpush.urls')),
+     path('', views.index),
 ]
