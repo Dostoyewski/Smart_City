b3297a2735a30dba8c71f54d3d6a1ab4da347935
Dostoyewski <dostoyewski@yandex.ru>
2020-04-27 15:17:19 +0300

    Added custom templates

--- smart_city/urls.py
+++ smart_city/urls.py
@@ -15,14 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
+from django.conf.urls import url, re_path
 from allauth.account import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
+    # url(r'^accounts/', include('allauth.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='account/login.html')),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='account/logout.html')),
     path('accounts/signup/', auth_views.SignupView.as_view(template_name='account/signup.html')),
-    path('accounts/signup/', auth_views.SignupView.as_view(template_name='account/signup.html')),
+    path('accounts/password/change/', auth_views.PasswordChangeView.as_view(template_name=
+                                                                            'account/password_change.html')),
+    path('accounts/password/set/', auth_views.PasswordSetView.as_view(template_name='account/password_set.html')),
+    path('accounts/inactive/', auth_views.AccountInactiveView.as_view(template_name='account/account_inactive.html')),
+    path('accounts/email/', auth_views.EmailView.as_view(template_name='account/email.html')),
+    path('accounts/confirm-email/', auth_views.ConfirmEmailView.as_view(template_name='account/email_confirm.html')),
+    path('accounts/password/reset/', auth_views.PasswordResetView.as_view(template_name='account/password_reset.html')),
+    path('accounts/password/reset/done/', auth_views.PasswordResetDoneView.as_view(template_name=
+                                                                                   'account/password_reset_done.html')),
+    path('accounts/password/reset/key/done/',
+         auth_views.PasswordResetFromKeyDoneView.as_view(template_name='account/password_reset_from_key_done.html')),
+    path('accounts/password/reset/', auth_views.PasswordResetView.as_view(template_name='account/password_reset.html')),
+    re_path(r"^confirm-email/(?P<key>[-:\w]+)/$", auth_views.ConfirmEmailView,
+            name="account_confirm_email"),
+    re_path(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+            auth_views.PasswordResetFromKeyView,
+            name="account_reset_password_from_key"),
 ]
