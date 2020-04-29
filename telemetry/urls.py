569ec6d2b320ffd35d490c4810307bc3ceeb5f88
Dostoyewski <dostoyewski@yandex.ru>
2020-04-29 20:59:16 +0300

    Fixed updated_db function

--- telemetry/urls.py
+++ telemetry/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.main_telemetry_page, name='telemetry_main'),
     path('critical/', views.data_critical, name='telemetry_critical'),
     path('danger/', views.data_attention, name='telemetry_danger'),
-    path('all/', views.data_all, name='telemetry_all')
+    path('all/', views.data_all, name='telemetry_all'),
+    path('update/', views.update_db, name='telemetry_update')
 ]
